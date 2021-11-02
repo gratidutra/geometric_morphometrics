fd3cc6c19980d269f15019da9b3e5a6c6f16eb31
gratidutra <gratirodrigues.gdr@gmail.com>
2021-11-01 22:29:02 -0300

    add new grafic

--- morpho_4.0.R
+++ morpho_4.0.R
@@ -173,7 +173,9 @@ pca_tt$x
 
 sink("PCA.txt")
 
-summary(pca_tt)
+summary <- summary(pca_tt)
+
+summary$PC.summary
 
 sink()
 
@@ -210,6 +212,77 @@ pca_plot <- ggplot(data = a, aes(x = Comp1, y = Comp2, colour = tab_pop)) +
 
 pca_plot
 
+## Plot 2 - PCA
+
+# Nosso dados
+
+especimes <- rownames(a)
+rownames(a) <- NULL
+data <- cbind(especimes, a) 
+data <- data %>% select(-tab_pop)%>% 
+  pivot_longer(!especimes, names_to = 'PC', values_to = 'value')
+
+
+lista <- double(26)
+pc_names <- list() 
+for (i in 1:26){
+  lista[[i]] <- summary[["PC.summary"]][[3,i]]
+  pc_names[[i]] <- paste0('PC_', i)
+}
+
+pc_df <- cbind(pc_names, lista)
+pc_df <- as.data.frame(pc_df, row) %>% 
+  mutate(id = row_number()) %>%
+  rename(value = lista)
+
+
+label_data_2 <- pc_df
+
+# calculate the ANGLE of the labels
+number_of_bar <- nrow(pc_df)
+angle <-  90 - 360 * (label_data_2$id-0.5) /number_of_bar     # I substract 0.5 because the letter must have the angle of the center of the bars. Not extreme right(1) or extreme left (0)
+
+# calculate the alignment of labels: right or left
+# If I am on the left part of the plot, my labels have currently an angle < -90
+label_data_2$hjust <-ifelse( angle <- 90, 1, 0)
+
+# flip angle BY to make them readable
+label_data_2$angle<-ifelse(angle <-90, angle+180, angle)
+# ----- ------------------------------------------- ---- #
+
+# Start the plot
+p <- ggplot(pc_df, aes(x=as.factor(id), y=as.numeric(value))) +       # Note that id is a factor. If x is numeric, there is some space between the first bar
+  
+  # This add the bars with a blue color
+  geom_bar(stat="identity", fill=alpha("skyblue", 0.7)) +
+  
+  # Limits of the plot = very important. The negative value controls the size of the inner circle, the positive one is useful to add size over each bar
+  ylim(0,1) +
+  
+  # Custom the theme: no axis title and no cartesian grid
+  theme_minimal() +
+  theme(
+    axis.text = element_blank(),
+    axis.title = element_blank(),
+    panel.grid = element_blank(),
+    plot.margin = unit(rep(-1,4), "cm"),
+    # Adjust the margin to make in sort labels are not truncated!
+  ) +
+  
+  # This makes the coordinate polar instead of cartesian.
+  coord_polar(start = 0) +
+  
+  # Add the labels, using the label_data dataframe that we have created before
+  geom_text(data=label_data_2, 
+            aes(x=id, y=as.numeric(value), label=pc_names, hjust=hjust), 
+            color="black", fontface="bold",alpha=0.6, 
+            size=2.5, 
+            angle= 90, 
+            inherit.aes = FALSE 
+  ) 
+
+p
+
 # Anova
 
 anova <- procD.lm(alg_t_all ~ tab_pop, iter = 9999, RRPP = TRUE)
@@ -243,6 +316,7 @@ cva.1 <- CVA(alg_t_all, tab_pop,
   rounds = 1000, cv = T,
   p.adjust.method = "bonferroni"
 )
+
 
 CVscores <- data.frame(cva.1$CVscores)
 
@@ -284,3 +358,5 @@ cva_plot <- ggplot(data = CVscores, aes(x = CV.1, y = CV.2, colour = tab_pop)) +
   )
 
 cva_plot
+
+
